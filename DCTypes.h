40fefb925ccc01314b8f74c82c350bf1b56ec8ce
ldandersen <ldandersen>
2004-12-30 00:17:55 +0000

    Added missing DCTypes header file (sorry everybody!).

---
+++ DCTypes.h
@@ -0,0 +1,11 @@
+/*
+ *  DCTypes.h
+ *  Delicious Client
+ *
+ *  Created by Laurence Andersen on Thu Dec 23 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+typedef enum { DCBasicSearchType = 0, DCExtendedSearchType = 1, DCFullTextSearchType = 2 } DCSearchType;
+
